828f2871b3db1f03002411d47872e67fbde307ef
amrit <rit.regmi@gmail.com>
2021-03-03 23:48:54 +0200

    testing hostname

--- frontend/src/Services/apiBaseUrl.ts
+++ frontend/src/Services/apiBaseUrl.ts
@@ -1,6 +1,7 @@
 /**Function to store apiBaseUrl to local storage  */
 export const setBaseApiBaseUrlToHost = ():void => {
   let apibaseUrl = process.env.HOSTNAME || window.location.host
+  console.log(apibaseUrl,process.env.HOSTNAME)
   if((window.location.host.includes('localhost'))){
 
     apibaseUrl = 'http://localhost:8080'
